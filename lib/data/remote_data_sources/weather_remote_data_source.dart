595750c5c6439b2baad0e6c763d50d31ff2a9299
michalcukrowski <micukr23@gmail.com>
2025-03-09 13:52:38 +0100

    Add .fromJson methode to our model

--- lib/data/remote_data_sources/weather_remote_data_source.dart
+++ lib/data/remote_data_sources/weather_remote_data_source.dart
@@ -1,11 +1,7 @@
 import 'package:dio/dio.dart';
 
-
 class WeatherRemoteDataSource {
-
-
-  
-  Future<Map<String , dynamic>?> getWeatherData({
+  Future<Map<String, dynamic>?> getWeatherData({
     required String city,
   }) async {
     // http://api.weatherapi.com/v1/current.json?key=0b2ada6bad544522a92115226250903&q=Luxembourg &aqi=no
--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 
 import 'package:simple_weather/data/remote_data_sources/weather_remote_data_source.dart';
 import 'package:simple_weather/domain/models/weather_model.dart';
@@ -10,15 +11,13 @@ class WeatherRepository {
     required String city,
   }) async {
     // http://api.weatherapi.com/v1/current.json?key=0b2ada6bad544522a92115226250903&q=Luxembourg &aqi=no
-    final responseData = await _weatherRemoteDataSource.getWeatherData(
+    final json = await _weatherRemoteDataSource.getWeatherData(
       city: city,
     );
-    if (responseData == null) {
+    if (json == null) {
       return null;
     }
-    final name = responseData['location']['name'] as String;
-    final temperature = (responseData['current']['temp_c']) + 0.0 as double;
 
-    return WeatherModel(city: name, temperature: temperature);
+    return WeatherModel.fromJson(json);
   }
 }
