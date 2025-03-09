fbfddab9312789cdfd5b0fd402c74d6d4e237773
michalcukrowski <micukr23@gmail.com>
2025-03-09 14:07:30 +0100

    Add Dio handling errors

--- lib/data/remote_data_sources/weather_remote_data_source.dart
+++ lib/data/remote_data_sources/weather_remote_data_source.dart
@@ -4,10 +4,15 @@ class WeatherRemoteDataSource {
   Future<Map<String, dynamic>?> getWeatherData({
     required String city,
   }) async {
-    // http://api.weatherapi.com/v1/current.json?key=0b2ada6bad544522a92115226250903&q=Luxembourg &aqi=no
+    try {
+      // http://api.weatherapi.com/v1/current.json?key=0b2ada6bad544522a92115226250903&q=Luxembourg &aqi=no
     final response = await Dio().get<Map<String, dynamic>>(
         'http://api.weatherapi.com/v1/current.json?key=0b2ada6bad544522a92115226250903&q=$city&aqi=no');
 
     return response.data;
+    } on DioError catch(error){
+      throw Exception(error.response?.data['error']['message'] ?? 'Unknow error');
+    }
+    
   }
 }
--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:simple_weather/data/remote_data_sources/weather_remote_data_source.dart';
 import 'package:simple_weather/domain/models/weather_model.dart';
 
@@ -10,7 +8,6 @@ class WeatherRepository {
   Future<WeatherModel?> getWeatherModel({
     required String city,
   }) async {
-    // http://api.weatherapi.com/v1/current.json?key=0b2ada6bad544522a92115226250903&q=Luxembourg &aqi=no
     final json = await _weatherRemoteDataSource.getWeatherData(
       city: city,
     );
